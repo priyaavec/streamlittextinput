2c2de964c211b9954eae45ee29eef15e4e60b1e9
priyaavec <78856816+priyaavec@users.noreply.github.com>
2021-04-02 11:08:54 +0100

    Update forminput.py

--- forminput.py
+++ forminput.py
@@ -1,16 +1,16 @@
 import streamlit as st
 
-    customer = "John"
-    customer_password = "4321"
-    employee = "Tom"
-    emppassword = "1234"
-    st.title("Text input")
-    option = st.selectbox('Please select a template?', ('Employee', 'Customer'))
-    if option == 'Employee':
-        st.write("Your option is Employee template")
-        st.text_input("User name", employee)
-        st.text_input("Password", emppassword)
-    else:
-        st.write("Your option is Customer template")
-        st.text_input("User name", customer)
-        st.text_input("Password",customer_password)
+customer = "John"
+customer_password = "4321"
+employee = "Tom"
+emppassword = "1234"
+st.title("Text input")
+option = st.selectbox('Please select a template?', ('Employee', 'Customer'))
+if option == 'Employee':
+    st.write("Your option is Employee template")
+    st.text_input("User name", employee)
+    st.text_input("Password", emppassword)
+else:
+    st.write("Your option is Customer template")
+    st.text_input("User name", customer)
+    st.text_input("Password",customer_password)
